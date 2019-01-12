a918e11cd97f674e967b017c1b58dd7f94123d57
Rachel Hawa <rachel.hawa@gmail.com>
2019-01-12 02:02:07 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,10 @@
 class Backer
-  attr_accessor :name
-  @@backed_projects = []
+  attr_accessor :name, :backed_projects
+
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
   def back_project(project)
